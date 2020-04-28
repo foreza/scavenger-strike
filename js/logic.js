4db643886ddf07d4703091742939e8d6b8ddb7a9
Doki <jforeza@gmail.com>
2020-04-27 20:49:08 -0700

    Initial skeleton commit with most major bootstrap components/basic layout

---
+++ js/logic.js
@@ -0,0 +1,34 @@
+
+
+let gameCarouselRef;
+
+let gameStartIndex = 0;                                 
+let gameCurrentIndex = 0;
+
+$( () => {
+
+    // Carousel must not automatically move. We don't want that.
+    gameCarouselRef = $('#game-carousel').carousel({
+        interval: 0,
+        keyboard: false
+      })
+
+})
+
+
+function carouselNavigateStart() {
+    gameCarouselRef.carousel(0);
+}
+
+function carouselNavigateToNext(){
+    gameCarouselRef.carousel('next');
+}
+
+
+
+
+$("#answer-0").click(() => {
+    // if answer is correct
+    // do some "sexy" animation then
+    carouselNavigateToNext();
+})
